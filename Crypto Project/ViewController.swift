45bbbe52a6b4fa5e91b39aa2bb59bfab62d09e1f
Akın <akincetiin19@gmail.com>
2022-11-29 17:35:08 +0300

    no message

---
+++ Crypto Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Crypto Project
+//
+//  Created by Akın on 29.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("adkpoaskdop")
+    }
+    
+
+}
+
